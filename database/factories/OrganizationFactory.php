60be0344a301fe84d2393e5c8eebc12a2714c5ac
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-14 12:40:09 +0300

    Created Organization model, migration, and factory

---
+++ database/factories/OrganizationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class OrganizationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'website' => $this->faker->url(),
+            'address' => $this->faker->streetAddress(),
+            'description' => $this->faker->paragraphs(2, true),
+        ];
+    }
+}
---
+++ database/migrations/2022_05_14_091734_create_organizations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('website');
+            $table->string('address');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+};
