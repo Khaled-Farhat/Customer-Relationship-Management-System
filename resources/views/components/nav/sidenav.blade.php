40cc94fae42790db80df66e643fe0370ffcde048
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-16 16:18:29 +0300

    Designed the navigation

--- resources/views/components/nav/sidenav.blade.php
+++ resources/views/components/nav/sidenav.blade.php
@@ -1,24 +1,26 @@
 <nav class="navbar position-fixed navbar-expand-lg bg-dark d-flex align-items-start h-100">
-    <div class="container px-4">
-        <ul class="mx-auto navbar-nav flex-column">
-            <li class="nav-item bg-primary bg-gradient rounded px-2">
-                <a class="nav-link text-white ps-2 pe-5" aria-current="page" href="#">
-                    <i class="bi bi-house-door-fill me-2"></i>
-                    Dashboard
-                </a>
-            </li>
-            <li class="nav-item px-2">
-                <a class="nav-link text-white ps-2 pe-5" href="#">
-                    <i class="bi bi-person-fill me-2"></i>
-                    Clients
-                </a>
-            </li>
-            <li class="nav-item px-2">
-                <a class="nav-link text-white ps-2 pe-5" href="#">
-                    <i class="bi bi-door-open-fill me-2"></i>
-                    Organizations
-                </a>
-            </li>
-        </ul>
-    </div>
+  <div class="container px-4">
+    <ul class="navbar-nav flex-column mx-auto">
+      <li class="nav-item @if ($activeNavItem === 'dashboard') bg-primary bg-gradient @endif rounded px-2">
+        <a class="nav-link ps-2 pe-5 text-white" aria-current="page" href="#">
+          <i class="bi bi-house-door-fill me-2"></i>
+          Dashboard
+        </a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link @if ($activeNavItem === 'clients') bg-primary bg-gradient @endif ps-2 pe-5 text-white"
+          href="{{ route('clients.index') }}">
+          <i class="bi bi-person-fill me-2"></i>
+          Clients
+        </a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link @if ($activeNavItem === 'organizations') bg-primary bg-gradient @endif ps-2 pe-5 text-white"
+          href="{{ route('organizations.index') }}">
+          <i class="bi bi-door-open-fill me-2"></i>
+          Organizations
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
--- resources/views/components/nav/topnav.blade.php
+++ resources/views/components/nav/topnav.blade.php
@@ -1,20 +1,23 @@
-<nav class="navbar shadow-sm bg-dark sticky-top">
-    <div class="container">
-        <a class="navbar-brand text-white" href="">CRM</a>
+<nav class="navbar bg-dark sticky-top shadow-sm">
+  <div class="container">
+    <a class="navbar-brand text-white" href="">CRM</a>
 
-        <li class="dropdown">
-            <a class="dropdown-toggle text-white text-decoration-none" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Khaled Farhat
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li>
-                    <a class="dropdown-item" href="#"><i class="bi bi-person-fill me-2"></i>Profile</a>
-                </li>
-                <li><hr class="dropdown-divider"></li>
-                <li>
-                    <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-left me-2"></i>Sign out</a>
-                </li>
-            </ul>
+    <li class="dropdown">
+      <a class="dropdown-toggle text-decoration-none text-white" href="#" id="navbarDropdown" role="button"
+        data-bs-toggle="dropdown" aria-expanded="false">
+        {{ auth()->user()->name }}
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li>
+          <a class="dropdown-item" href="#"><i class="bi bi-person-fill me-2"></i>Profile</a>
         </li>
-    </div>
+        <li>
+          <hr class="dropdown-divider">
+        </li>
+        <li>
+          <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-left me-2"></i>Sign out</a>
+        </li>
+      </ul>
+    </li>
+  </div>
 </nav>
