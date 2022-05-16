4f0281c193de94593609d420b57054a993de5c6d
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-16 17:23:47 +0300

    Added foreign key constraints to clients table

--- database/migrations/2022_05_14_094054_create_clients_table.php
+++ database/migrations/2022_05_14_094054_create_clients_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('organization_id');
+            $table->foreignId('organization_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
