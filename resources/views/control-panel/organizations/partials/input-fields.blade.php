c10376c4cca867dad162cf5fe53398166027ae8d
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-17 11:35:12 +0300

    Fixed the margins in organization input fields

--- resources/views/control-panel/organizations/partials/input-fields.blade.php
+++ resources/views/control-panel/organizations/partials/input-fields.blade.php
@@ -1,4 +1,12 @@
-<x-forms.text name="name" label="Name" :value="optional($organization)->name" class="mb-3" />
-<x-forms.text name="address" label="Address" :value="optional($organization)->address" class="mb-3" />
-<x-forms.text name="website" label="Website" :value="optional($organization)->website" class="mb-3" />
-<x-forms.textarea name="description" label="Description" :value="optional($organization)->description" class="mb-3" />
+<div class="mb-3">
+  <x-forms.text name="name" label="Name" :value="optional($organization)->name" />
+</div>
+<div class="mb-3">
+  <x-forms.text name="address" label="Address" :value="optional($organization)->address" />
+</div>
+<div class="mb-3">
+  <x-forms.text name="website" label="Website" :value="optional($organization)->website" />
+</div>
+<div class="mb-3">
+  <x-forms.textarea name="description" label="Description" :value="optional($organization)->description" />
+</div>
