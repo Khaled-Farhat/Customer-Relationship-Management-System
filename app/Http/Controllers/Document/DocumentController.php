ca30b001787f069e7d54a884136f54c3f9f258d4
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-19 20:27:25 +0300

    Created DocumentController

---
+++ app/Http/Controllers/Document/DocumentController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Document;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Document\StoreDocumentRequest;
+use App\Http\Requests\Document\UpdateDocumentRequest;
+use App\Models\Document;
+use App\Services\DocumentService;
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, Document $document, DocumentService $documentService)
+    {
+        return $documentService->download($request, $document);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Document\UpdateDocumentRequest  $request
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDocumentRequest $request, Document $document)
+    {
+        $document->name = $request->name;
+        $document->save();
+
+        session()->flash('success', 'Document renamed successfully');
+
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Document $document)
+    {
+        $document->delete();
+
+        session()->flash('success', 'Document deleted successfully');
+
+        return redirect()->back();
+    }
+}
