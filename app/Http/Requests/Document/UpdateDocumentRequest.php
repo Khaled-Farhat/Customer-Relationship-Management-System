e3242b6da828f82a4efed99a0c15111d4c44af0f
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-19 19:52:20 +0300

    Created requests classes for storing and updating documents

---
+++ app/Http/Requests/Document/UpdateDocumentRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Document;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateDocumentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                Rule::unique('media')->where(function($query) {
+                    return $query
+                                ->where('model_type', $this->route('document')->model_type)
+                                ->where('model_id', $this->route('document')->model_id)
+                                ->where('name', $this->name);
+                })->ignore($this->route('document')),
+            ],
+        ];
+    }
+}
