b8d05a43cfba01be48ceddab0c8dd8c4f9bb2ab9
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-05-19 20:03:43 +0300

    Added a setter to created_at attribute in the Document model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Document extends Media
 {
-
+    public function getCreatedAtAttribute($createdAt)
+    {
+        return Carbon::parse($createdAt)->format('Y/m/d - H:i');
+    }
 }
