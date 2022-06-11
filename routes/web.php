9c4e3ed2a6d6552ebedbe0fb4e973522e4f3565f
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-06-11 14:21:38 -0400

    Added web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Client\ClientController;
+use App\Http\Controllers\Client\ClientDocumentController;
+use App\Http\Controllers\Document\DocumentController;
+use App\Http\Controllers\Organization\OrganizationController;
+use App\Http\Controllers\Organization\OrganizationClientController;
+use App\Http\Controllers\Organization\OrganizationDocumentController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +21,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', 'login');
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function() {
+    Route::resource('users', UserController::class);
+
+    Route::resource('organizations', OrganizationController::class);
+    Route::resource('organizations.clients', OrganizationClientController::class)->only(['index']);
+    Route::resource('organizations.documents', OrganizationDocumentController::class)->only(['index', 'store']);
+
+    Route::resource('clients', ClientController::class);
+    Route::resource('clients.documents', ClientDocumentController::class)->only(['index', 'store']);
+
+    Route::resource('documents', DocumentController::class)->only(['show', 'update', 'destroy']);
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
