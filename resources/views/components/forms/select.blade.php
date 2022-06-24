7f642e88f6d117b8462fcf1f653674bfa5b8ac48
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-06-24 14:56:20 +0300

    Modified the form select component to show feedback for invalid input

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -1,14 +1,19 @@
-@props(['name', 'label' => null, 'options', 'selected' => null])
+@props(['name', 'label' => null, 'options', 'selected' => null, 'hasFeedback' => true])
 
 @if ($label)
   <x-forms.label :name="$name" :label="$label" />
 @endif
 
-<select name={{ $name }} {{ $attributes->merge([
-    'class' => 'form-select',
-]) }}>
+<select name={{ $name }}
+  {{ $attributes->merge([
+      'class' => 'form-control' . ($hasFeedback && $errors->has($name) ? ' is-invalid' : ''),
+  ]) }}>
   @foreach ($options as $key => $value)
     <option value="{{ $value }}" @if ($value === $selected) selected @endif>{{ $key }}
     </option>
   @endforeach
 </select>
+
+@if ($hasFeedback)
+  <x-forms.invalid-feedback :name="$name" />
+@endif
