9dd64ef7ae139effd528077291888a0b4b24728b
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2022-06-25 19:58:18 +0300

    Updated the 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,11 @@
-@extends('layouts.control-panel')
+@extends('layouts.app', [
+    'noTopNav' => true,
+])
 
-@section('content')
-  <div class="card text-center p-3">
-    <div class="card-body mx-auto">
-      <h1 class="card-title mb-4">404 | Page not found</h1>
-      <h6 class="card-subtitle text-muted mb-2">The page you were looking for does not exist</h6>
-    </div>
+<div class="card m-4 p-5 text-center">
+  <div class="card-body mx-auto">
+    <h1 class="card-title mb-4">404 | Page not found</h1>
+    <h6 class="card-subtitle text-muted mb-4">The page you were looking for does not exist</h6>
+    <a class="btn btn-primary" href="{{ route('dashboard') }}">Home</a>
   </div>
-@endsection
+</div>
